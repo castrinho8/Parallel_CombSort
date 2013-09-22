d7c6e72cb483afd87199a25a2653f4fabff8ff8e
castrinho8 <pablo.castro1@udc.es>
2013-09-22 20:43:56 +0200

    Cambiada la estructura del proyecto y añadida la licencia

--- src/combsorthibrido.c
+++ src/combsorthibrido.c
@@ -1,8 +1,14 @@
-/*
- * Practica Final AEC. FIC. UDC. 2012/2013
- *  Pablo Castro Valiño (pablo.castro1@udc.es)
- *  Marcos Chavarría Teijeiro (marcos.chavarria@udc.es)
- */
+/***************************************************************************
+ *	Practica Final AEC. FIC. UDC. 2012/2013											*
+ *	Copyright (C) 2013 by Pablo Castro and Marcos Chavarria 						*
+ * <pablo.castro1@udc.es>, <marcos.chavarria@udc.es> 								*
+ * 																								*
+ * This program is free software; you can redistribute it and/or modify 	*
+ * it under the terms of the GNU General Public License as published by 	*
+ * the Free Software Foundation; either version 2 of the License, or 		*
+ * (at your option) any later version. 												*
+ ***************************************************************************/
+
 #include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
